6374209155831475804538533091afd318f67ed5
Jozef Knaperek <jknaperek@gmail.com>
2017-09-19 09:31:59 +0200

    Release version 0.16.9

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name='djangosaml2',
-    version='0.16.8',
+    version='0.16.9',
     description='pysaml2 integration for Django',
     long_description='\n\n'.join([read('README.rst'), read('CHANGES')]),
     classifiers=[
