d5263f9c1f93836f05de7cd3116d2859b917fb14
Jozef Knaperek <jknaperek@gmail.com>
2018-07-16 18:06:14 +0200

    Fix release

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if sys.version_info < (3, 4):
 
 setup(
     name='djangosaml2',
-    version='0.17.0',
+    version='0.17.1',
     description='pysaml2 integration for Django',
     long_description='\n\n'.join([read('README.rst'), read('CHANGES')]),
     classifiers=[
