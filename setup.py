5561d114c8721abde6d7b9967e9cc36732bcfb9d
Jozef Knaperek <jknaperek@gmail.com>
2018-08-29 13:36:32 +0200

    Release version 0.17.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if sys.version_info < (3, 4):
 
 setup(
     name='djangosaml2',
-    version='0.17.1',
+    version='0.17.2',
     description='pysaml2 integration for Django',
     long_description='\n\n'.join([read('README.rst'), read('CHANGES')]),
     classifiers=[
